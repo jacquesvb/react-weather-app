16eabfe983e44f111c97fe025f17e82ef5d3c715
Jacques Van Blokland <jacquesvb@live.com>
2024-05-23 14:28:32 -0700

    Forecast

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { FaReact } from 'react-icons/fa';
-import TopButtons from './components/TopButtons';
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TempAndDetails from "./components/TempAndDetails";
+import Forecast from "./components/Forecast";
 
 const App = () => {
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
       <TopButtons />
+      <Inputs />
+
+      <TimeAndLocation />
+      <TempAndDetails />
+      <Forecast />
+      <Forecast />
     </div>
-  )
-}
+  );
+};
 
 export default App;
