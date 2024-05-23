a58ef552624afd3403b1ad1060f2b902bf642ca7
Jacques Van Blokland <jacquesvb@live.com>
2024-05-23 15:15:35 -0700

    Api data

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails";
 import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
 
 const App = () => {
+  const getWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "Berlin" });
+    console.log(data);
+  };
+  getWeather();
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
       <TopButtons />
