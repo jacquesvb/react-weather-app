86fa05d3b715f245e742ddf24999c00e2c26fcb9
Jacques Van Blokland <jacquesvb@live.com>
2024-05-24 20:47:36 -0700

    Fixed weatherservice

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -48,7 +48,37 @@ const formatCurrent = (data) => {
     details,
     icon: iconUrlFromCode(icon),
     formattedLocalTime,
+    dt,
+    timezone,
+    lat,
+    lon,
   };
+};
+
+const formatForecastWeather = (secs, offset, data) => {
+  console.log(secs);
+  // hourly
+  const hourly = data
+    .filter((f) => f.dt > secs)
+    .map((f) => ({
+      temp: f.main.temp,
+      title: formatToLocalTime(f.dt, offset, "hh:mm a"),
+      icon: iconUrlFromCode(f.weather[0].icon),
+      date: f.dt_txt,
+    }))
+    .slice(0, 5);
+
+  // daily
+  const daily = data
+    .filter((f) => f.dt_txt.slice(-8) === "00:00:00")
+    .map((f) => ({
+      temp: f.main.temp,
+      title: formatToLocalTime(f.dt, offset, "ccc"),
+      icon: iconUrlFromCode(f.weather[0].icon),
+      date: f.dt_txt,
+    }));
+
+  return { hourly, daily };
 };
 
 const getFormattedWeatherData = async (searchParams) => {
@@ -57,7 +87,15 @@ const getFormattedWeatherData = async (searchParams) => {
     searchParams
   ).then(formatCurrent);
 
-  return { ...formattedCurrentWeather };
+  const { dt, lat, lon, timezone } = formattedCurrentWeather;
+
+  const formattedForecastWeather = await getWeatherData("forecast", {
+    lat,
+    lon,
+    units: searchParams.units,
+  }).then((d) => formatForecastWeather(dt, timezone, d.list));
+
+  return { ...formattedCurrentWeather, ...formattedForecastWeather };
 };
 
 export default getFormattedWeatherData;
