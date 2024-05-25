25692e644efea597dbf546428af74ebacc8885a3
Jacques Van Blokland <jacquesvb@live.com>
2024-05-25 00:45:02 -0700

    Search works

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,25 @@ const App = () => {
     await getFormattedWeatherData({ ...query, units }).then((data) => {
       setWeather(data);
     });
-    console.log(data);
   };
 
   useEffect(() => {
     getWeather();
   }, [query, units]);
 
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-600 to-blue-700";
+    const threshold = units === "metric" ? 20 : 68;
+    if (weather.temp <= threshold) return "from-cyan-600 to-blue-700";
+    return "from-yellow-600 to-orange-700";
+  };
+
   return (
-    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
+    <div
+      className={`mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
       <TopButtons setQuery={setQuery} />
-      <Inputs />
+      <Inputs setQuery={setQuery} setUnits={setUnits} />
 
       {weather && (
         <>
